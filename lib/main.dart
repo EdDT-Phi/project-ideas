9797ac217b3b255ed6a9b4394b6bcabe7a24cad3
Eddie <eddt.phi@gmail.com>
2018-03-21 23:13:01 -0700

    Database support, add, edit and view ideas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+
+final DatabaseReference reference =
+    FirebaseDatabase.instance.reference().child('ideas');
 
 void main() => runApp(new MyApp());
 
@@ -18,8 +23,6 @@ class ProjectIdeas extends StatefulWidget {
 }
 
 class ProjectIdeasState extends State<ProjectIdeas> {
-  final _ideas = <String>['idea1', 'idea2', 'idea3', 'idea4'];
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -33,14 +36,21 @@ class ProjectIdeasState extends State<ProjectIdeas> {
   }
 
   Widget _projectsList() {
-    return new ListView.builder(
-      itemBuilder: (context, i) {
-        if (i.isOdd) return new Divider();
-        final index = i ~/ 2;
-        return new ProjectIdeaRow(text: _ideas[index]);
+    return new FirebaseAnimatedList(
+      query: reference,
+      itemBuilder:
+          (_, DataSnapshot snapshot, Animation<double> animation, int n) {
+        return _buildRow(snapshot);
       },
-      itemCount: _ideas.length * 2,
     );
+  }
+
+  Widget _buildRow(DataSnapshot snapshot) {
+    return new ListTile(
+        title: new Text(snapshot.value['name']),
+        onTap: () {
+          _viewIdea(snapshot);
+        });
   }
 
   void _newIdea() {
@@ -48,29 +58,36 @@ class ProjectIdeasState extends State<ProjectIdeas> {
       return new EditIdea();
     }));
   }
-}
 
-class ProjectIdeaRow extends StatelessWidget {
-  ProjectIdeaRow({this.text});
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return new ListTile(title: new Text(text));
+  void _viewIdea(DataSnapshot snapshot) {
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      return new ViewIdea(snapshot: snapshot);
+    }));
   }
 }
 
 class EditIdea extends StatefulWidget {
+  EditIdea({this.snapshot});
+  final DataSnapshot snapshot;
+
   @override
-  createState() => new EditIdeaState();
+  createState() => new EditIdeaState(snapshot: snapshot);
 }
 
 class EditIdeaState extends State<EditIdea> {
+  EditIdeaState({this.snapshot});
   final formKey = new GlobalKey<FormState>();
-  String _username;
+  DataSnapshot snapshot;
+  String _name = '';
+  String _description = '';
 
   @override
   Widget build(BuildContext context) {
+    if (snapshot != null) {
+      _name = snapshot.value['name'];
+      _description = snapshot.value['description'];
+    }
+
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Project Ideas'),
@@ -88,8 +105,16 @@ class EditIdeaState extends State<EditIdea> {
         children: <Widget>[
           new TextFormField(
             decoration: new InputDecoration(labelText: 'Idea Name'),
+            initialValue: _name,
             validator: (val) => val.isEmpty ? 'Name can\'t be empty.' : null,
-            onSaved: (val) => _username = val,
+            onSaved: (val) => _name = val,
+          ),
+          new TextFormField(
+            decoration: new InputDecoration(labelText: 'Description'),
+            initialValue: _description,
+            validator: (val) =>
+                val.isEmpty ? 'Description can\'t be empty.' : null,
+            onSaved: (val) => _description = val,
           ),
         ],
       ),
@@ -101,7 +126,61 @@ class EditIdeaState extends State<EditIdea> {
     if (form.validate()) {
       form.save();
 
-      // TODO(eddie): Save to database.
+      DatabaseReference newIdea;
+      if(snapshot == null) {
+        newIdea = reference.push();
+      } else {
+        newIdea = reference.child(snapshot.key);
+      }
+
+      newIdea.set({
+        'name': _name,
+        'description': _description,
+      });
+
+      newIdea.once().then((snapshot) {
+        Navigator
+            .of(context)
+            .pushReplacement(new MaterialPageRoute(builder: (context) {
+          return new ViewIdea(snapshot: snapshot);
+        }));
+      });
     }
+  }
+}
+
+class ViewIdea extends StatelessWidget {
+  ViewIdea({this.snapshot});
+  final DataSnapshot snapshot;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Project Ideas'),
+      ),
+      body: _buildView(),
+      floatingActionButton: new FloatingActionButton(
+          onPressed: () {
+            _editIdea(context);
+          },
+          child: new Icon(Icons.edit)),
+    );
+  }
+
+  Widget _buildView() {
+    return new Column(
+      children: <Widget>[
+        new ListTile(
+          title: new Text(snapshot.value['name']),
+        ),
+      ],
+    );
+  }
+
+  void _editIdea(BuildContext context) {
+    Navigator.of(context).pushReplacement(new MaterialPageRoute(builder: (context) {
+      return new EditIdea(snapshot: snapshot);
+    }));
   }
 }
